ca1ea29fa77dc960f4bc085e706ed9fe5ef5529d
Erick Chandra <erickchandra@Ericks-MacBook-Air.local>
2022-10-14 21:09:57 +0800

    improve react modularity and fix csss for coding page

--- src/components/BlockCoding.js
+++ src/components/BlockCoding.js
@@ -1,100 +1,115 @@
 export default function BlockCodingHeader() {
 
-    const ProjectDetails = [
-        {
-            title: "Website: Ascenda Loyalty Program Marketplace",
-            description: "Ascenda Loyalty operates an API based Loyalty Points Marketplace which facilitates easy and seamless loyalty currency transfers between multiple loyalty programs. Customers of our loyalty points marketplace product are typically loyalty programs that want to offer their customers abilities toconvert their own loyalty currency to a variety of other third party loyalty currencies.You will be able to find many examples of such loyalty programs locally here in Singapore. Forexample, GrabRewards, which is a loyalty program for Grab, offers the ability for one to convert Grabpoints to airline currencies, such as Singapore Airlines KrisFlyer or AirAsia Big.",
-            contribution: "In this project, my contribution is heavily on backend site with only one frontend feature"
-        },
-        {
-            title: "Android App Developments : Smart Canteen @SUTD",
-            description: " An easy-to-use mobile application which allows people to check what dishes are available today and whether the stalls are still open. By allowing users to indicate their interest in buying from the stall, the stall owners can in turn see how many people are expected to turn up. This is a win-win situation as students and guests can see what foods are available in the canteen without having to visit the canteen physically, while the stall owners can gauge how many batches of food to make and decrease food waste.",
-            contribution: "Automated image uploading and storing system into Firebase Storage which also returned the cloud URL link that is passed to the Firebase Realtime Database. Moreover, I implemented the database management class where the added menu details and images can be automatically displayed inside the Frontend RecyclerView User Interface",
-        },
-        {
-            title: "Machine Learning : Covid-19 Death Prediction",
-            description: "",
-            contribution: "Cleaned the World Covid-19 Data sourced from Kaggle with Pandas and Numpy• Built a Linear regression model with 90 percent accuracy against the number of total cases, total tests,vaccination rates, population, without implementing Scikit Library"
-        },
-    ]
-
-    const CompetitionDetails = [
-        {
-            title: "Third Place in DBS Bank Design and Artificial Intelligence Challenge",
-            description: "Competition held by SUTD and DBS Bank to predict promising credit card",
-            contribution: "Proposed a credit card to target young working adults aged 25-33 which has the highest tendency to acquire new credit cards based on the Machine Learning Training Model using Scikit Library"
-        },
-   
-    ]
-
-    
-
     return (
-        <section>       
-        <div className="container mx-auto flex px-10 pt-20 md:flex-row flex-col items-center">
-            <div className=" md:w-1/2 w-1/3 lg:pr-24 md:pr-16 md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-                <h1 className="text-left pb-10 pl-20 text-4xl">
-                    <b>Group Projects</b>
-                </h1>
-            </div>
-            <div className="flex-col">
-                {ProjectDetails.map((project) => {
-                    const { title, description, contribution } = project;
-                    return (
-                        <BlockCodingContent
-                            title={title}
-                            description={description}
-                            contribution={contribution}
-                        />
-                    )
-                })
-                }
-            </div>
-        </div>
-        <div className="container mx-auto flex px-10 pt-20 md:flex-row flex-col items-center">
-            <div className=" md:w-1/2 lg:pr-24 md:pr-16 md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-                <h1 className="text-left mb-10 ml-20 text-4xl">
-                    <b>Competitions</b>
-                </h1>
-            </div>
-            <div className="flex-col">
-                {ProjectDetails.map((project) => {
-                    const { title, description, contribution } = project;
-                    return (
-                        <BlockCodingContent
-                            title={title}
-                            description={description}
-                            contribution={contribution}
-                        />
-                    )
-                })
-                }
-            </div>
-        </div>
-        <div className="container mx-auto flex px-10 pt-20 md:flex-row flex-col items-center">
-            <div className="lg:flex md:w-1/2 w-1/2 lg:pr-24 md:pr-16 md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-                <h1 className="text-left pb-10 pl-20 text-4xl">
-                    <b>Group Projects</b>
-                </h1>
-            </div>
-            <div className="flex-col">
-                {ProjectDetails.map((project) => {
-                    const { title, description, contribution } = project;
-                    return (
-                        <BlockCodingContent
-                            title={title}
-                            description={description}
-                            contribution={contribution}
-                        />
-                    )
-                })
-                }
-            </div>
-        </div>
+        <section>
+
+            {TitleLists.map((detail)=>{
+                const{header,detailsName}=detail;
+                return(
+                    <FullBlock
+                        header = {header}
+                        detailsName={detailsName}
+                    />
+                )
+                
+            })}
+           
         </section>
-        
+
     )
 }
+
+const ProjectDetails = [
+    {
+        title: "Website: Ascenda Loyalty Program Marketplace",
+        description: "Ascenda Loyalty operates an API based Loyalty Points Marketplace which facilitates easy and seamless loyalty currency transfers between multiple loyalty programs. Customers of our loyalty points marketplace product are typically loyalty programs that want to offer their customers abilities toconvert their own loyalty currency to a variety of other third party loyalty currencies.You will be able to find many examples of such loyalty programs locally here in Singapore. Forexample, GrabRewards, which is a loyalty program for Grab, offers the ability for one to convert Grabpoints to airline currencies, such as Singapore Airlines KrisFlyer or AirAsia Big.",
+        contribution: "In this project, my contribution is heavily on backend site with only one frontend feature"
+    },
+    {
+        title: "Android App Developments : Smart Canteen @SUTD",
+        description: " An easy-to-use mobile application which allows people to check what dishes are available today and whether the stalls are still open. By allowing users to indicate their interest in buying from the stall, the stall owners can in turn see how many people are expected to turn up. This is a win-win situation as students and guests can see what foods are available in the canteen without having to visit the canteen physically, while the stall owners can gauge how many batches of food to make and decrease food waste.",
+        contribution: "Automated image uploading and storing system into Firebase Storage which also returned the cloud URL link that is passed to the Firebase Realtime Database. Moreover, I implemented the database management class where the added menu details and images can be automatically displayed inside the Frontend RecyclerView User Interface",
+    },
+    {
+        title: "Machine Learning : Covid-19 Death Prediction",
+        description: "",
+        contribution: "Cleaned the World Covid-19 Data sourced from Kaggle with Pandas and Numpy• Built a Linear regression model with 90 percent accuracy against the number of total cases, total tests,vaccination rates, population, without implementing Scikit Library"
+    },
+]
+
+const CompetitionDetails = [
+    {
+        title: "Third Place in DBS Bank Design and Artificial Intelligence Challenge",
+        description: "Competition held by SUTD and DBS Bank to predict promising credit card",
+        contribution: "Proposed a credit card to target young working adults aged 25-33 which has the highest tendency to acquire new credit cards based on the Machine Learning Training Model using Scikit Library"
+    },
+
+]
+const OutsideProjectDetails = [
+    {
+        title: "PPIS Tech Team",
+        description: "Indonesia Community in Singapore",
+        contribution: "Constructed a new backup MySQL database and integrating it with Wix API to automate member application processes and removal of expired members’ dataImplementing new User Interface site with ReactJs  for membership info retrieval as a first step to migrate website from Wix to a new domain"
+    },
+
+]
+const PersonalProjectDetails = [
+    {
+        title: "Third Place in DBS Bank Design and Artificial Intelligence Challenge",
+        description: "Competition held by SUTD and DBS Bank to predict promising credit card",
+        contribution: "Proposed a credit card to target young working adults aged 25-33 which has the highest tendency to acquire new credit cards based on the Machine Learning Training Model using Scikit Library"
+    },
+
+]
+const TitleLists = [
+    {
+        header: "Group Projects",
+        detailsName: ProjectDetails,
+    },
+    {
+        header: "Competitions",
+        detailsName: CompetitionDetails,
+    },
+    {
+        header: "Outside of Schools",
+        detailsName: OutsideProjectDetails,
+    },
+    {
+        header: "Personal Projects",
+        detailsName: PersonalProjectDetails,
+    },
+]
+function FullBlock(props) {
+    const {
+        header,
+        detailsName
+    } = props;
+
+
+    return (
+        <div className="container pl-10 flex px-10 pt-20 md:flex-row flex-col-2 items-center">
+            <div className=" pl-20  w-[300px] shrink-0 lg:pr-24  mb-16  text-left">
+                <h1 className="pb-10 text-4xl">
+                    <b>{header}</b>
+                </h1>
+            </div>
+            <div className=" ml-20 flex-col">
+                {detailsName.map((project) => {
+                    const { title, description, contribution } = project;
+                    return (
+                        <BlockCodingContent
+                            title={title}
+                            description={description}
+                            contribution={contribution}
+                        />
+                    )
+                })
+                }
+            </div>
+        </div>
+    )
+
+};
 
 function BlockCodingContent(props) {
     const {
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,51 +1,74 @@
 import React from "react";
 // import Dropdown from 'react-bootstrap-dropdown';
 export default function Navbar() {
+
+    const NavigationLists =[
+      {
+        name:"Home",
+        linkref:"/Home",
+      },
+      {
+        name:"More Info",
+        linkref:"/About",
+      },
+      {
+        name:"Coding",
+        linkref:"/Coding",
+      },
+      {
+        name:"Photography",
+        linkref:"/Photography",
+      },
+      {
+        name:"Venture Capital",
+        linkref:"/VentureCapital",
+      },
+      {
+        name:"Coffee",
+        linkref:"/Coffee",
+      },
+      {
+        name:"Referral Code",
+        linkref:"/ReferralCode",
+      },
+      {
+        name:"Contact",
+        linkref:"/Contact",
+      },
+      {
+        name:"Bunch Of Others",
+        linkref:"/Hobbies",
+      }
+    ];
+
+    function NavComponents(props){
+      const{
+        name,
+        linkref
+      }= props
+
+      return(
+        <a href={linkref} className="mr-5 hover:text-black">
+        {name}
+        </a>
+      );
+    };
+
     return (
       <header className="bg-sky-900 md:sticky top-0 z-10">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center text-white">
-          {/* <a className="title-font font-medium text-white mb-4 md:mb-0">
-            <a href="/About" className="ml-3 text-xl">
-              Reed Barger
-            </a>
-          </a> */}
           <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-          <a href="/Home" className="mr-5 hover:text-black">
-              Home
-            </a>
-            <a href="/About" className="mr-5 hover:text-black">
-              More Info
-            </a>
-            
-            <a href="/Coding" className="mr-5 hover:text-black">
-              Coding
-            </a>
-            <a href="/Photography" className="mr-5 hover:text-black">
-              Photography
-            </a>
-            <a href="/VentureCapital" className="mr-5 hover:text-black">
-              Venture Capital
-            </a>
-            <a href="/Coffee" className="mr-5 hover:text-black">
-              Coffee
-            </a>
-            <a href="/ReferralCode" className="mr-5 hover:text-black">
-              Referral Code
-            </a>
-            <a href="/Hobbies" className="mr-5 hover:text-black">
-              Too many Other Hobbies
-            </a>
-            <a href="/Contact" className="mr-5 hover:text-black">
-              Contact
-            </a>
-            
+          
+          {NavigationLists.map((component)=>{
+            const {name,linkref}= component
+            return(
+              <NavComponents
+                name = {name}
+                linkref = {linkref}
+              />
+            );
+          })}
           </nav>
-          {/* <a
-            href="#contact"
-            className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
-            Hire Me
-            <ArrowRightIcon className="w-4 h-4 ml-1" />
-          </a> */}
         </div>
       </header>
     );
